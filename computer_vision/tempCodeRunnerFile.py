1456bbc4be9ddbf8fb52d1ccebf7720cdc9be813
yasmineben510 <75130336+yasmineben510@users.noreply.github.com>
2024-05-31 14:30:25 +0200

    trying to display the complete image while playing

--- computer_vision/tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-    for x in range(number_grid_lines):
-            start_point = intersections[x][0]
-            end_point = intersections[x + 2*number_grid_lines][0]
-            cv2.line(image, start_point, end_point, (0, 0, 255), 2)
